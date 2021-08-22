72196364b6c3dc8740d1563c393bffbd450ba5f3
laurenkan. <laurenkan.@localhost>
2021-08-22 14:31:35 -0700

    ye 8/22/21

--- src/_05_netflix/Netflix.java
+++ src/_05_netflix/Netflix.java
@@ -6,10 +6,17 @@ public class Netflix {
 		  Movie movie = new Movie("very intersting movie title", 5);
 		 movie.getRating();
 		 movie.getTicketPrice();
-		System.out.println(movie.getTitle());
-		 System.out.println(movie.getRating());
+		System.out.println("The movie title is: " + movie.getTitle());
+		 System.out.println("The rating of " + movie.getTitle() + " is " + movie.getRating());
 		 System.out.println(movie.getTicketPrice());
-		//okay..
+		 System.out.println("The second best movie is... ");
+		 System.out.println("The second movie is... ");
+		 ///System.out.println(movie.compareTo(otherMovie));
+		int popcorn;
+		int soda;
+		int hiddensnacks;
+		
+		
 	}
 
 }
